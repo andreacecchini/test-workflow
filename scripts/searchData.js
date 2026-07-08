7f74bd9cbe04804e4efef46e1289afb0155a1da7
andreacecchini <andreacecchini@users.noreply.github.com>
2026-07-08 16:06:30 +0000

    deploy: fc55375d7ecdcdf25178bbbd3c6e0dd0e759573a

---
+++ scripts/searchData.js
@@ -0,0 +1,5 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"scala3-template","t":"scala3-template","d":"","k":"static","x":""},
+{"l":"$lessempty$greater$/Main$.html#","e":false,"i":"","n":"Main","t":"Main","d":"","k":"object","x":""},
+{"l":"$lessempty$greater$/Main$.html#greetingMessage-0","e":false,"i":"","n":"greetingMessage","t":"greetingMessage: String","d":"Main","k":"def","x":""},
+{"l":"$lessempty$greater$/Main$.html#main-913","e":false,"i":"","n":"main","t":"main(args: Array[String]): Unit","d":"Main","k":"def","x":""},
+{"l":"$lessempty$greater$/Main$.html#projectName-0","e":false,"i":"","n":"projectName","t":"projectName: String","d":"Main","k":"val","x":""}];
